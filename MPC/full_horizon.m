4e711811b7b0069fdc2f90e626a189ae46b03c34
FrancescoBerruti <117366119+FrancescoBerruti@users.noreply.github.com>
2024-11-16 11:11:00 +0100

    Aggiunti i commenti

--- MPC/full_horizon.m
+++ MPC/full_horizon.m
@@ -9,6 +9,7 @@ function [mf_eq,I,SOC,mf]= full_horizon(u,SOC_0,StateUpdate)
     for i=2:N+1
         [mf(i),SOC(i),seq(i),mb(i),I(i)]=StateUpdate(u(i-1),SOC(i-1),i);
     end
+    %Funzione di costo un po' tumore, ma funziona sembra :)
     mf_eq=sum(mf(2:end).*seq(2:end)+abs(0.55-SOC(end)./(seq(2:end)+(seq(2:end)==0))));
     SOC=SOC(2:end);
 end
